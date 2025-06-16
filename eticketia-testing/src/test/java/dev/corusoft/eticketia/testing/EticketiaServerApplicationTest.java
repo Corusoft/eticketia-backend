a38e55e35f2b15685a3c1f6eab406482dd2831cf
Javier <javier.c.mato@gmail.com>
2025-06-17 00:58:14 +0200

    Estructuración de módulos
    - Configuración básica de spring en cada submódulo
    - Configuración de Log4j2
    - Perfiles y dependencias entre módulos

--- eticketia-testing/src/test/java/dev/corusoft/eticketia/testing/EticketiaServerApplicationTest.java
+++ eticketia-testing/src/test/java/dev/corusoft/eticketia/testing/EticketiaServerApplicationTest.java
@@ -3,10 +3,12 @@ package dev.corusoft.eticketia.testing;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import dev.corusoft.eticketia.server.EticketiaServerApplication;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Log4j2
 @SpringBootTest(classes = {EticketiaServerApplication.class})
 class EticketiaServerApplicationTest {
 
@@ -14,7 +16,8 @@ class EticketiaServerApplicationTest {
   private EticketiaServerApplication serverApplication;
 
   @Test
-  void contextLoads() {
+  void springTestContextLoads() {
     assertThat(serverApplication).isNotNull();
+    log.debug("Application context loaded succesfuly for testing environment.");
   }
 }
