4e36b459df0823410802a2107fab9e82508f3b5c
Javier <javier.c.mato@gmail.com>
2025-06-20 21:23:03 +0200

    chore: coverage and logging configurations

--- eticketia-server/src/main/java/dev/corusoft/eticketia/server/EticketiaServerApplication.java
+++ eticketia-server/src/main/java/dev/corusoft/eticketia/server/EticketiaServerApplication.java
@@ -1,5 +1,6 @@
 package dev.corusoft.eticketia.server;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Application entry point.
  */
 @SpringBootApplication
+@Log4j2
 public class EticketiaServerApplication {
 
   /**
